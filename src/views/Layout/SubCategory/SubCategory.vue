7fe773891281f019fe226b2bca19e4262768c66d
John <ae411253@gmail.com>
2024-09-12 16:14:41 +0800

    內容區無限加載功能實現

--- src/views/Layout/SubCategory/SubCategory.vue
+++ src/views/Layout/SubCategory/SubCategory.vue
@@ -29,20 +29,50 @@ getSubCategoryList()
 
 const subCategoryContent = ref([]) // 存儲二級分類列表內容
 
+
 const contentData = ref({ // 用來傳遞參數
   categoryId : route.params.id, // 二級分類 ID
   page: 1, // 頁數
   pageSize: 20, // 單頁多少條數據
-  sortField: 'publishTime' // 排序方式 ( 這裡默認是最新商品 )
+  sortField: '' || 'publishTime'// 排序方式 ( 這裡默認是最新商品 )
 })
 
 const getSubCategoryContent = async () => {
   const res = await getSubContent(contentData.value)
   subCategoryContent.value = res.result.items
 }
-
 getSubCategoryContent()
 
+// 篩選功能處理 
+const tabChange = () => {
+  // 讓當前頁數回到第一頁
+  contentData.value.page = 1
+  // 重新調用接口 獲取新的數據 
+  getSubCategoryContent()
+}
+
+// 定義scroll 監聽 默認為 false 
+const onDisabled = ref(false)
+
+//  頁面無限加載事件
+const getNewList = async () => {
+  
+  //  滾動到底部時 讓頁數++ 達成無限加載
+  contentData.value.page++
+  
+  //  再次發送請求  獲取新的數據 
+  const res = await getSubContent(contentData.value)
+
+  //  此時 res 拿到的就是新的數據了 , 接下來進行數據拼接 (這裡不用數組 contain方法 , 用展開運算符來拼接 )
+  subCategoryContent.value = [...subCategoryContent.value, ...res.result.items] 
+  
+  // 當後端沒有數據的時候 , 停止監聽 ( 防止性能浪費 )
+  if ( res.result.items.length === 0 ) {
+     onDisabled.value = true
+     contentData.value.page = 1
+  }
+  
+}
 
 </script>
 
@@ -58,12 +88,12 @@ getSubCategoryContent()
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="contentData.sortField" @tab-change="tabChange" >
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人氣" name="orderNum"></el-tab-pane>
         <el-tab-pane label="評論最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="getNewList" :infinite-scroll-disabled="onDisabled">
          <!-- 二級分類列表內容-->
         <GoodsItem v-for="good in subCategoryContent" :good="good" :key="good.id" />
       </div>
