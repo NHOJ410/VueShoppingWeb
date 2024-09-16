a1a4de0a5ded455c677fba72607c0deb395766b7
John <ae411253@gmail.com>
2024-09-16 11:01:46 +0800

    token過期/失效 的統一處理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { ElMessage } from 'element-plus'
+import 'element-plus/dist/index.css'
+import { ElMessage , ElMessageBox } from 'element-plus'
+import router from '@/router'
 // 導入user倉庫 
 import { useUserInfoStore } from '@/stores'
 
@@ -43,7 +45,36 @@ httpInstance.interceptors.response.use(function (response) {
   // 超出 2xx 範圍的狀態碼都會觸發該函數。
   // 對響應錯誤做點什麼
 
+  // token過期/失效處理 , 當響應狀態碼是401的時候
+  if ( error.response.status === 401 ) {
+    
+    // 調用清除用戶信息的方法
+    const userStore = useUserInfoStore()
+    userStore.clearUserInfo()
+    
+    // 使用確認框組件詢問用戶是否要跳轉到登入頁面
+    ElMessageBox.confirm(
+      '你好像還沒有登入 ! 請問要跳轉到登入頁面嗎',
+      '溫馨提示',
+      {
+        confirmButtonText: '跳轉到登入頁面',
+        cancelButtonText: '我再逛逛,系統將自動跳轉回首頁',
+        type: 'warning',
+      }
+    ).then(() => { // 跳轉到登入頁面邏輯
+        router.push('/login')
+      })
+      .catch(() => { // 停留在未登入狀態的邏輯
+      // 重新回到首頁
+      router.push('/')
+    })
+
+    return
+  }
+
   ElMessage.error(error.response?.data.message || '服務器錯誤!')
+
+  
 
   return Promise.reject(error);
 });
